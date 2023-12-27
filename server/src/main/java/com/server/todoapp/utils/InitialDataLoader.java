3949b9bb4929f59c2a71240b599763c768977245
MihaiV0 <mihai.vulcan@student.upt.ro>
2023-12-27 16:12:20 +0200

    InitialDataLoader

---
+++ server/src/main/java/com/server/todoapp/utils/InitialDataLoader.java
@@ -0,0 +1,45 @@
+package com.server.todoapp.utils;
+
+import com.server.todoapp.domain.entity.User;
+import com.server.todoapp.domain.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.mindrot.bcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InitialDataLoader implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String ddlAuto;
+
+    @Value("${security.bcrypt.salt}")
+    private String bcryptSalt;
+
+    public InitialDataLoader(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    @Override
+    public void run(String... args) throws Exception {
+        if (ddlAuto.equals("create")) {
+            User user = createUser("edi pop",
+                    "edi.pop@gmail.com",
+                    BCrypt.hashpw("AppAdmin123@", bcryptSalt));
+            userRepository.save(user);
+        }
+    }
+
+    private User createUser(String username, String email, String hashPassword) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(hashPassword);
+        user.setUsername(username);
+
+        return user;
+    }
+}
